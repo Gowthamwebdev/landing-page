7edfd89e757ff72e13a6dcf2e0f9c869625dbe72
Gowtham <137925188+Gowthamwebdev@users.noreply.github.com>
2024-08-22 16:23:37 +0530

    style(frontend): home- before img added

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,7 @@
 import Watch1 from "../assets/watch1.png";
 import  announcement  from "../assets/announcement.png";
+import GolfCourseIcon from '@mui/icons-material/GolfCourse';
+import RasmikaImg from "../assets/rasmika.webp";
 
 const Home = () => {
   return (
@@ -11,14 +13,14 @@ const Home = () => {
             <div className="w-[50px] h-[50px] sm:w-[100px] sm:h-[100px] rounded-full">
               <img src={announcement} alt="" />
             </div>
-              <h2 className='text-5xl sm:text-6xl lg:text-7xl font-bold bg-gradient-to-b from-slate-700 to-slate-950 text-transparent bg-clip-text'>Upto 50% off on selected products</h2>
+              <h2 className='text-5xl sm:text-6xl lg:text-7xl font-bold bg-gradient-to-b from-slate-700 to-slate-950 text-transparent bg-clip-text'>Upto 50% off* with excited prices</h2>
               <p className="text-[0.6rem] sm:text-sm lg:text-lg sm:max-w-lg font-mono text-slate-600 ">Embrace timeless elegance this summer with our exquisite analog watches.</p>
               <div className="flex py-4">
-              <a className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">Order now</a>
+              <a className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-md">Order now</a>
               </div>
               <div className="flex w-3/4">
-                <div className="bg-orange-400 rounded-lg flex  flex-row">
-                    <img src={Watch1} alt="" className="w-[5rem] h-[5rem] mt-2"/>
+                <div className="bg-orange-500 rounded-lg flex  flex-row">
+                <GolfCourseIcon sx={{color:'white', height:'3rem', width:'3rem'}} />
                 <div className="text-white p-4">
                 <h2 className="font-sans font-semibold pb-2">100+ Collections</h2>
                 <p className="font-sans sm:max-w-[12rem]">More than 1lakh happy customers</p>
@@ -29,6 +31,10 @@ const Home = () => {
           <div className="flex flex-col justify-center pt-12 sm:pt-0 text-center sm:text-left relative z-10 gap-4">
             <div>
               <img src={Watch1} alt="" className="w-[300px] h-[300px] sm:w-[450px] sm:h-[450px] sm:scale-125 object-contain mx-auto"/>
+              <div className="h-[100px] w-[100px] bg-gradient-to-t from-orange-600 to-red-800 absolute left-0 bottom-5 rounded-lg -z[8]">
+                <img src={RasmikaImg} alt="" className=" object-contain w-full h-full" />
+              </div>
+              <div className="h-[100px] w-[100px] bg-primary absolute right-10 bottom[-10]0 rounded-full -z[8]"></div>
             </div>
           </div>
         </div>
